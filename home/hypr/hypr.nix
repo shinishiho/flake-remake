734b9478778082cb02252204eabfcdeb1ee60340
Đỗ Quang Minh <59284549+shinishiho@users.noreply.github.com>
2024-02-09 22:17:58 +0700

    Update hypr.nix

--- home/hypr/hypr.nix
+++ home/hypr/hypr.nix
@@ -34,7 +34,7 @@
         follow_mouse = 1;
 
         touchpad = {
-          natural_scroll = no;
+          natural_scroll = false;
         };
 
         sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
