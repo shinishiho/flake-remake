6433ed4857da072c36b58cd210e2ff0fc2e616b7
ShiniShiho <nakxkri@shinishiho.cyou>
2024-02-09 22:08:14 +0700

    no monaspace for you

--- home/shell/kitty.nix
+++ home/shell/kitty.nix
@@ -3,10 +3,6 @@
 {
   programs.kitty = {
     enable = true;
-    font.package = [
-      pkgs.monaspace
-    ];
-    font.name = "Monaspace Krypton";
     font.size = 12;
     shellIntegration.enableZshIntegration = true;
   }
