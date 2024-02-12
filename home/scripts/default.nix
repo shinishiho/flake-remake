4c50e603694ed908794539ca7d5d93c1864050df
ShiniShiho <nakxkri@shinishiho.cyou>
2024-02-12 10:37:09 +0700

    skip script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -3,6 +3,6 @@
 {
   home.packages = [
     (import ./test.nix { inherit pkgs; })
-    (import ./theme.nix { inherit pkgs; inherit config; })
+    #(import ./theme.nix { inherit pkgs; inherit config; })
   ];
 }
